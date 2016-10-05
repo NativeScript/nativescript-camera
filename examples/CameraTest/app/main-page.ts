3abc3d0c5dbfc570656d18ce55dde1de091488d7
Nedyalko Nikolov <nedyalko.nikolov@telerik.com>
2016-10-05 15:02:08 +0300

    Added CameraTest example.

---
+++ examples/CameraTest/app/main-page.ts
@@ -0,0 +1,32 @@
+import { EventData, Observable, fromObject } from "data/observable";
+import { Page } from "ui/page";
+import { View } from 'ui/core/view';
+import { takePicture, requestPermissions } from "nativescript-camera";
+import * as appModule from "application";
+
+import * as trace from "trace";
+trace.addCategories(trace.categories.Debug);
+trace.enable();
+
+export function navigatingTo(args: EventData) {
+    var page = <Page>args.object;
+    let picturePath = null;
+
+    page.bindingContext = fromObject({cameraImage: picturePath, saveToGallery: true});
+}
+
+export function onRequestPermissionsTap(args: EventData) {
+    requestPermissions();
+}
+
+export function onTakePictureTap(args: EventData) {
+    let page = <Page>(<View>args.object).page;
+    let saveToGallery = page.bindingContext.get("saveToGallery");
+    takePicture({saveToGallery: saveToGallery}).
+        then((imageAsset) => {
+            page.bindingContext.set("cameraImage", imageAsset);
+        }, 
+        (err) => {
+            console.log("Error -> " + err.message);
+        });
+}
---
+++ examples/CameraTest/references.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="./node_modules/tns-core-modules/tns-core-modules.es6.d.ts" />
+
+/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
