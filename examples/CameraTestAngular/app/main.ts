f8da02e51fab503157311f2eb7463bfd015b1f82
vakrilov <alexander.vakrilov@telerik.com>
2017-04-07 11:29:31 +0300

    CameraTestAngular app + webpack + aot

--- examples/CameraTestAngular/app/main.ts
+++ examples/CameraTestAngular/app/main.ts
@@ -1,15 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { NativeScriptFormsModule } from "nativescript-angular/forms";
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
 
-@NgModule({
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-    imports: [NativeScriptModule, NativeScriptFormsModule],
-})
-class AppComponentModule {}
+import { AppModule } from "./app.module";
 
-platformNativeScriptDynamic().bootstrapModule(AppComponentModule);
+platformNativeScriptDynamic().bootstrapModule(AppModule);
