0b96ff1e8a24dea2db493f4cc1cc5c386b115716
Zdravko Branzov <Zdravko.Branzov@progress.com>
2018-05-25 17:02:18 +0300

    chore: add ui tests

---
+++ demo/e2e/tests.e2e.ts
@@ -0,0 +1,65 @@
+import { AppiumDriver, createDriver, SearchOptions, Direction, UIElement, Point, Locator } from "nativescript-dev-appium";
+import { isSauceLab, runType } from "nativescript-dev-appium/lib/parser";
+import { expect } from "chai";
+
+const isSauceRun = isSauceLab;
+const isAndroid: boolean = runType.includes("android");
+
+describe("Camera", () => {
+    let driver: AppiumDriver;
+
+    before(async () => {
+        driver = await createDriver();
+        driver.defaultWaitTime = 15000;
+    });
+
+    after(async () => {
+        if (isSauceRun) {
+            driver.sessionId().then(function (sessionId) {
+                console.log("Report: https://saucelabs.com/beta/tests/" + sessionId);
+            });
+        }
+        await driver.quit();
+        console.log("Driver quits!");
+    });
+
+    afterEach(async function () {
+        if (this.currentTest.state === "failed") {
+            await driver.logScreenshot(this.currentTest.title);
+        }
+    });
+
+    it("should take a picture", async function () {
+        const takePictureButton = await driver.findElementByText("Take Picture");
+        await takePictureButton.click();
+        if (isAndroid) {
+            let allow = await driver.findElementByTextIfExists("Allow", SearchOptions.exact);
+            if (allow !== undefined) {
+                await allow.click();
+                allow = await driver.findElementByTextIfExists("Allow", SearchOptions.exact);
+                await allow.click();
+            }
+            const deny = await driver.findElementByTextIfExists("Deny", SearchOptions.exact);
+            if (deny !== undefined) {
+                await deny.click();
+            }
+            let images = await driver.findElementsByClassName(driver.locators.image); // Take a picture
+            await images[5].click();
+            images = await driver.findElementsByClassName(driver.locators.image); // Accept it
+            await images[4].click();
+        } else {
+            let ok = await driver.findElementByTextIfExists("OK", SearchOptions.exact);
+            if (ok !== undefined) {
+                await ok.click();
+                ok = await driver.findElementByTextIfExists("OK", SearchOptions.exact);
+                await ok.click();
+            }
+            await driver.wait(2000);
+            await driver.clickPoint(50, 100); // Select directory
+            await driver.wait(2000);
+            await driver.clickPoint(50, 150); // Select image
+        }
+        const saveToGalleryLabel = await driver.findElementByText("saveToGallery");
+        expect(saveToGalleryLabel).to.exist;
+    });
+});
